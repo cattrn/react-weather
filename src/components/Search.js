cc6ac19757175d3fb06961faac07598c2203128d
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-07-21 10:45:08 +1000

    changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,24 @@
+import { useState } from "react"
+
 const Search = () => {
+  const [searchQuery, setSearchQuery] = useState("")
+
+  const searchHandler = e => {
+    setSearchQuery(e.target.value)
+  }
+
   return (
     <div className="search">
       <form>
-        <input type="text" id="search-bar" placeholder="Search cities" />
+        <input
+          type="text"
+          id="search-bar"
+          placeholder="Search cities"
+          onChange={searchHandler}
+        />
         <button type="submit">Search</button>
       </form>
-      {/* Result will appear here */}
+      <div className="search-results">{/* Result will appear here */}</div>
     </div>
   )
 }
